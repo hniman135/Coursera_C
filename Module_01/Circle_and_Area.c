a25f1384299e6efdd1aefaa377e7cc6a2fa38ee5
hniman135 <20521594@gm.uit.edu.vn>
2024-08-05 17:02:31 +0700

    5/8/2024

---
+++ Module_01/Circle_and_Area.c
@@ -0,0 +1,18 @@
+/*
+    Circle and Area
+        hniman 
+        Aug. 3, 2024
+*/
+#include <stdio.h>
+
+#define PI 3.14
+
+int main()
+{
+    double area = 0.0, radius = 0.0;
+    printf("Enter radius: ");
+    scanf("%lf", &radius);
+    area = PI * radius * radius;
+    printf("With %lf meters radius, the area equal %lf sq. meters.\n", radius, area);
+    return 0;
+}
