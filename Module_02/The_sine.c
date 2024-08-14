2baabce2707cb244bdbdc83874b4174ae5c74ea5
hniman135 <20521594@gm.uit.edu.vn>
2024-08-14 18:04:04 +0700

    Create The_sine.c

---
+++ Module_02/The_sine.c
@@ -0,0 +1,16 @@
+/*
+    The_sine
+        hniman 
+        Aug. 14, 2024
+*/
+
+#include <stdio.h>
+#include "math.h"
+int main()
+{
+    double x, sine;
+    printf ("Enter a value for x (0 <= x <= 1): \n");
+    scanf("%lf", &x);
+    sine = sin(x);
+    printf ("The sine of %lf is %lf\n", x, sine);
+}
